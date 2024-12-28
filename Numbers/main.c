e7315a8bbb419fa6946fda945da3048ef620badf
Edwin Cardenas <eacardenase@gmail.com>
2024-12-28 15:20:36 -0500

    feat: perform both float and integer division

--- Numbers/main.c
+++ Numbers/main.c
@@ -8,11 +8,9 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    unsigned long x = 255;
-    
-    printf("x is %lu.\n", x);
-    printf("In octal, x is %lo.\n", x);
-    printf("In hexadecimal, x is %lx.\n", x);
+    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
+    printf("11 / 3.0 = %f\n", 11 / (float)3);
     
     return 0;
 }
