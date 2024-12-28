470628c6624dfe87784a0f6a81c1bb41643f2393
Edwin Cardenas <eacardenase@gmail.com>
2024-12-28 15:32:02 -0500

    feat: get absolute value from a signed int

--- Numbers/main.c
+++ Numbers/main.c
@@ -6,11 +6,13 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
     printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
     printf("11 / 3.0 = %f\n", 11 / (float)3);
+    printf("The absolute value of -5 is %d\n", abs(-5));
     
     return 0;
 }
