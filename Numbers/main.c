bc61b9b0b00f205722f5fc3a406f914a515ae702
Edwin Cardenas <eacardenase@gmail.com>
2024-12-28 15:44:17 -0500

    feat: introduce floats

--- Numbers/main.c
+++ Numbers/main.c
@@ -9,10 +9,18 @@
 #include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
+    printf("Integers be like:\n\n");
+    
     printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
     printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
     printf("11 / 3.0 = %f\n", 11 / (float)3);
-    printf("The absolute value of -5 is %d\n", abs(-5));
+    printf("The absolute value of -5 is %d\n\n", abs(-5));
+    
+    printf("Floats be like:\n\n");
+    
+    double y = 12345.6789;
+    printf("y is %f\n", y);
+    printf("y is %e\n", y);
     
     return 0;
 }
